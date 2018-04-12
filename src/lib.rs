53959333b04794ad62a859be0be9313c58d772e4
Gergely Nagy <ngg@tresorit.com>
2018-04-12 16:01:29 +0200

    futures 0.2
    This reverts commit a60be4cafaacc7f03569b8865235fbf7043a142a.

--- src/lib.rs
+++ src/lib.rs
@@ -2,56 +2,42 @@
 #![recursion_limit = "4096"]
 
 extern crate proc_macro;
-extern crate proc_macro2;
 #[macro_use]
 extern crate quote;
 extern crate syn;
 
 use proc_macro::TokenStream;
-use proc_macro2::Span;
 use syn::{Ident, Item};
 
 #[proc_macro_attribute]
-pub fn async_test(attribute: TokenStream, function: TokenStream) -> TokenStream {
+pub fn async_test(params: TokenStream, function: TokenStream) -> TokenStream {
+    assert!(
+        params.to_string() == "",
+        "the #[async_test] attribute currently does not take parameters"
+    );
+
     let mut parsed = syn::parse::<Item>(function).expect("failed to parse tokens as a function");
+    let mut attrs = vec![];
     let (ident, inner_ident) = match parsed {
         Item::Fn(ref mut item) => {
             let orig = item.ident;
             let inner_name = "_inner_".to_owned() + orig.as_ref();
             item.ident = Ident::from(inner_name);
+            std::mem::swap(&mut attrs, &mut item.attrs);
             (orig, item.ident)
         }
         _ => panic!("#[async_test] can only be applied to functions"),
     };
 
-    let span = Span::call_site();
-    let call = match attribute.to_string().as_ref() {
-        "( should_panic )" => quote_spanned!(span=>
-            use std;
-            let result = std::panic::catch_unwind(|| #inner_ident().wait());
-            if result.is_ok() {
-                panic!("test did not panic");
-            }
-        ),
-        "( should_fail )" => quote_spanned!(span=>
-            let result = #inner_ident().wait();
-            if result.is_ok() {
-                panic!("test did not fail")
-            }
-        ),
-        "" => quote_spanned!(span=>
-            #inner_ident().wait().unwrap();
-        ),
-        _ => panic!("the #[async_test] attribute currently only takes `should_panic` or `should_fail` as an arg")
-    };
-
-    quote_spanned!(span=>
+    quote!(
         #[async]
         #parsed
 
         #[test]
+        #(#attrs )*
         fn #ident() {
-            #call
+            use futures::stable::block_on_stable;
+            block_on_stable(#inner_ident()).unwrap();
         }
     ).into()
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,29 +1,20 @@
-#![feature(generators, proc_macro)]
+#![feature(generators, pin, proc_macro)]
 
-extern crate futures_await as futures;
+extern crate futures;
 extern crate futures_await_test;
-use futures_await_test::async_test;
 use futures::prelude::*;
+use futures_await_test::async_test;
 
 fn create_future() -> impl Future<Item = u32, Error = ()> {
     Ok(4).into_future()
 }
 
-#[async_test(should_panic)]
+#[async_test]
+#[should_panic]
 fn panic_test() -> Result<(), ()> {
     let x = await!(create_future())?;
     assert!(x == 5);
     Ok(())
-}
-
-#[async_test(should_fail)]
-fn fail_test() -> Result<(), ()> {
-    let x = await!(create_future())?;
-    if x == 5 {
-        Ok(())
-    } else {
-        Err(())
-    }
 }
 
 #[async_test]
