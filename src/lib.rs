0f59be18e5d2f4d9aaaea6da2f4eef295715734d
Gergely Nagy <ngg@ngg.hu>
2019-03-05 11:42:41 +0100

    simplify, correct dep versions

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,22 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_quote, Ident, Item, Visibility};
+use syn::{parse_macro_input, parse_quote, Ident, Item, Visibility};
 
 #[proc_macro_attribute]
-pub fn async_test(params: TokenStream, function: TokenStream) -> TokenStream {
+pub fn async_test(params: TokenStream, input: TokenStream) -> TokenStream {
     assert!(
-        params.to_string() == "",
+        params.is_empty(),
         "the #[async_test] attribute currently does not take parameters"
     );
 
-    let mut inner = syn::parse::<Item>(function).expect("failed to parse tokens as a function");
+    let mut inner = parse_macro_input!(input as Item);
     let mut outer = inner.clone();
     if let (&mut Item::Fn(ref mut inner_fn), &mut Item::Fn(ref mut outer_fn)) =
         (&mut inner, &mut outer)
     {
         inner_fn.ident = Ident::new(
-            &("_inner_".to_owned() + &inner_fn.ident.to_string()),
+            &("_inner_".to_string() + &inner_fn.ident.to_string()),
             Span::call_site(),
         );
         let inner_ident = &inner_fn.ident;
