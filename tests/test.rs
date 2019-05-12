cb9fa14e659a78aa8fb7c28a3537f1673775f677
Gergely Nagy <ngg@ngg.hu>
2019-05-12 14:49:12 +0200

    do not use future::lazy

--- tests/test.rs
+++ tests/test.rs
@@ -1,33 +1,28 @@
 #![feature(async_await)]
 
-use futures::future::lazy;
-use futures::prelude::*;
 use futures_await_test::async_test;
 
-fn create_future() -> impl Future<Output = u32> {
-    lazy(|_| 4)
+async fn create_future() -> u32 {
+    4
 }
 
-fn create_result_future() -> impl Future<Output = Result<u32, ()>> {
-    lazy(|_| Ok(4))
+async fn create_result_future() -> Result<u32, ()> {
+    Ok(create_future().await)
 }
 
 #[async_test]
 #[should_panic]
 async fn panic_test() {
-    let x = create_future().await;
-    assert!(x == 5);
+    assert!(create_future().await == 5);
 }
 
 #[async_test]
 async fn normal_test() {
-    let x = create_future().await;
-    assert!(x == 4);
+    assert!(create_future().await == 4);
 }
 
 #[async_test]
 async fn result_test() -> Result<(), ()> {
-    let x = create_result_future().await?;
-    assert!(x == 4);
+    assert!(create_result_future().await? == 4);
     Ok(())
 }
